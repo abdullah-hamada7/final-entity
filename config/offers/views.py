153f9a4c519dc12299ecdc52257f64e203dcd812
abdullahhamada7 <chief.abdullah14@gmail.com>
2026-07-05 23:50:19 +0300

    feat: add login_required decorator to enhance security for offer viewss

--- config/offers/views.py
+++ config/offers/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 from django.contrib import messages
 from django.http import JsonResponse
